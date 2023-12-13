38a2b86b35655deaf93cf9d22b891cd09bf11c14
Ian Saari <ian.saari1@ucalgary.ca>
2023-12-13 11:08:17 -0700

    corrected comment typo

--- app/src/main/java/com/knollsoftware/orienteeringsymbols/data/SymbolsUiState.kt
+++ app/src/main/java/com/knollsoftware/orienteeringsymbols/data/SymbolsUiState.kt
@@ -3,7 +3,7 @@ package com.knollsoftware.orienteeringsymbols.data
 import com.knollsoftware.orienteeringsymbols.data.DataSource.symbols
 
 data class SymbolsUiState(
-    /** Selected symbol, used for specifying the symbol list scrollto item */
+    /** Selected symbol, used for specifying the symbol list scroll to item */
     val symbol: Symbol = symbols.first(),
     /** Indicator to highlight list item when clicking a grid symbol */
     val highlight: Boolean = false
