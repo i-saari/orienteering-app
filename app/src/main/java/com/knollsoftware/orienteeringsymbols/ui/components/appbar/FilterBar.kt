c86d5a21f9d579a67879982d72adcd892443f9dd
Ian Saari <ian.saari1@ucalgary.ca>
2024-06-27 18:01:17 -0700

    styled filter bar

--- app/src/main/java/com/knollsoftware/orienteeringsymbols/ui/components/appbar/FilterBar.kt
+++ app/src/main/java/com/knollsoftware/orienteeringsymbols/ui/components/appbar/FilterBar.kt
@@ -1,5 +1,6 @@
 package com.knollsoftware.orienteeringsymbols.ui.components.appbar
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -7,6 +8,8 @@ import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,9 +20,12 @@ fun FilterBar(
 ) {
     LazyRow(
         modifier = modifier
+            .padding(start = 4.dp, end = 4.dp)
     ) {
         items(filterItems) {
             FilterChip(
+                modifier = modifier
+                    .padding(4.dp),
                 selected = it.selected,
                 onClick = { onClick(it) },
                 label = { Text(it.group) }
@@ -32,3 +38,18 @@ data class FilterItem (
     val group: String,
     val selected: Boolean
 )
+
+@Preview
+@Composable
+fun FilterBarPreview() {
+    val items = listOf<FilterItem>(
+        FilterItem("Landforms", false),
+        FilterItem("Rock and Boulder", false),
+        FilterItem("Water and Marsh", false)
+    )
+    
+    FilterBar(
+        filterItems = items,
+        onClick = {}
+    )
+}
