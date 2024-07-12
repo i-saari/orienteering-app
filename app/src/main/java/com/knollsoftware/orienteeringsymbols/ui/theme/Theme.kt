9e1bcdfe6ca1099262f89be351ef84397ec4a8bd
Ian Saari <ian.saari1@ucalgary.ca>
2024-07-11 19:46:45 -0700

    added status bar color

--- app/src/main/java/com/knollsoftware/orienteeringsymbols/ui/theme/Theme.kt
+++ app/src/main/java/com/knollsoftware/orienteeringsymbols/ui/theme/Theme.kt
@@ -3,7 +3,6 @@ package com.knollsoftware.orienteeringsymbols.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -97,12 +96,12 @@ fun OrienteeringSymbolsTheme(
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
-//        SideEffect {
-//            val window = (view.context as Activity).window
-////            window.statusBarColor = colorScheme.primary.toArgb()
-////            window.statusBarColor = colorScheme.surface.toArgb()
-//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-//        }
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+//            window.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
